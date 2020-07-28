124f26a8f40151614588cefe5da524c1f4d664de
VUBANG <vubangbee@gmail.com>
2020-07-28 18:53:44 +0700

    Create Game Bau Cua

---
+++ src/GameBauCua/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class GameBauCua extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                GameBauCua
+
+            </div>
+        )
+    }
+}
+
+export default GameBauCua
